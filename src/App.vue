9103f5b60d6950770fcc082dccf707e8c957040d
Fidel Ernesto Alonso Ochoa <fidehlg89@gmail.com>
2021-01-31 14:46:33 -0500

    development task list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TasksList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TasksList from './components/TasksList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TasksList
   }
 }
 </script>
